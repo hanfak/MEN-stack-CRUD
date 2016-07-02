8af83f64bb279c85308f052ffd1f3c8b11bce828
Hanif Fakira <fakira.work@gmail.com>
2016-07-02 23:24:27 +0100

    setup handlebars and views, layout and main

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require("express");
-var exampleText = require("./exampleText.js");
+// var exampleText = require("./exampleText.js");
+var hbs =require("express-handlebars");
 
 var app = express();
 
 app.get("/", function(req, res){
-  res.send(exampleText);
+  res.send('Hello World!');
 });
 
 app.listen(3001, function(){
