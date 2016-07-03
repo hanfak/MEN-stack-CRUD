098624b7354451d2fc011fd7ab496ac74abbc31c
Hanif Fakira <fakira.work@gmail.com>
2016-07-03 13:41:59 +0100

    added schema

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,16 @@
+var mongoose  = require("mongoose");
+
+var PlayersSchema = new mongoose.Schema(
+  {
+    name: String,
+    goals: Number
+  }
+);
+
+mongoose.model("Players", PlayersSchema);
+mongoose.connect("mongodb://localhost/players");
+
+
 var seedData = require("./seeds.json");
 module.exports = {
   players: seedData
